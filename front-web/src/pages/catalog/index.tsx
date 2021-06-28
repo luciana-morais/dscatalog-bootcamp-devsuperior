689cc86d9e0303b59fe699c40b34c5f29d994c18
luciana-morais <lucianamorais01@yahoo.com.br>
2021-06-28 20:38:03 -0300

    Add product listing with backend

--- front-web/src/pages/catalog/index.tsx
+++ front-web/src/pages/catalog/index.tsx
@@ -1,26 +1,43 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import ProductCard from './components/productCard';
 import './styles.scss';
+import { makeRequest } from '../../core/utils/request';
+import { useState } from 'react';
+import { ProductsResponse } from '../../core/types/Product';
 
-const Catalog = () => (
+const Catalog = () => {
+
+const [productsResponse, setProductsResponse] = useState<ProductsResponse>();
+
+console.log(productsResponse);
+ 
+    useEffect(() => {
+
+        const params = {
+            page: 0,
+            linesPerPage: 12            
+        }
+
+        makeRequest ({url: '/products', params})
+        .then(response => setProductsResponse(response.data));
+    }, [])
+ 
+    return (
     <div className="catalog-container">
         <h1 className="catalog-title">
             Catálogo de produtos
         </h1>    
     <div className="catalog-products">  
-        <Link to="/products/1"><ProductCard /></Link>
-        <Link to="/products/2"><ProductCard /></Link>
-        <Link to="/products/3"><ProductCard /></Link>
-        <Link to="/products/4"><ProductCard /></Link>
-        <Link to="/products/5"><ProductCard /></Link>
-        <Link to="/products/6"><ProductCard /></Link>
-        <Link to="/products/7"><ProductCard /></Link>
-        <Link to="/products/8"><ProductCard /></Link>
-        <Link to="/products/9"><ProductCard /></Link> 
+        {productsResponse?.content.map(product => (
+            <Link to={`/products/${product.id}`} key={product.id}>
+                <ProductCard product={product} />]
+            </Link>
+        ))} 
     </div>
 </div>
-);
+)
+};
 
 
 export default Catalog;
